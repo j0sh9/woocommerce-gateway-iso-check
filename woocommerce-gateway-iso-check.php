99c1300e8f7fc898e5f85c6fd9cba88aa6581e8a
j0sh9 <jbphotoaz@gmail.com>
2017-10-23 14:34:40 -0700

    Update woocommerce-gateway-iso-check.php
    Add Bulk Edit Action

--- woocommerce-gateway-iso-check.php
+++ woocommerce-gateway-iso-check.php
@@ -540,3 +540,73 @@ function add_isocheck_hold_to_order_statuses( $order_statuses ) {
     return $new_order_statuses;
 }
 add_filter( 'wc_order_statuses', 'add_isocheck_hold_to_order_statuses' );
+/*
+ * Add your custom bulk action in dropdown
+ * @since 3.5.0
+ */
+add_filter( 'bulk_actions-edit-shop_order', 'isocheck_status_register_bulk_action' ); // edit-shop_order is the screen ID of the orders page
+ 
+function isocheck_status_register_bulk_action( $bulk_actions ) {
+ 
+	$bulk_actions['mark_isocheck_hold'] = 'Mark eCheck Hold'; // <option value="mark_awaiting_shipment">Mark awaiting shipment</option>
+	return $bulk_actions;
+ 
+}
+ 
+/*
+ * Bulk action handler
+ * Make sure that "action name" in the hook is the same like the option value from the above function
+ */
+add_action( 'admin_action_mark_isocheck_hold', 'isocheck_bulk_process_custom_status' ); // admin_action_{action name}
+ 
+function isocheck_bulk_process_custom_status() {
+ 
+	// if an array with order IDs is not presented, exit the function
+	if( !isset( $_REQUEST['post'] ) && !is_array( $_REQUEST['post'] ) )
+		return;
+ 	
+	$current_user = wp_get_current_user();
+	
+	foreach( $_REQUEST['post'] as $order_id ) {
+ 
+		$order = new WC_Order( $order_id );
+		$order_note = 'Order updated to eCheck Funding Hold using Bulk Edit by '.$current_user->display_name;
+		$order->update_status( 'isocheck-hold', $order_note, true ); // "misha-shipment" is the order status name (do not use wc-misha-shipment)
+ 
+	}
+ 
+	// of course using add_query_arg() is not required, you can build your URL inline
+	$location = add_query_arg( array(
+    	'post_type' => 'shop_order',
+		'marked_isocheck_hold' => 1, // markED_awaiting_shipment=1 is just the $_GET variable for notices
+		'changed' => count( $_REQUEST['post'] ), // number of changed orders
+		'ids' => join( $_REQUEST['post'], ',' ),
+		'post_status' => 'all'
+	), 'edit.php' );
+ 
+	wp_redirect( admin_url( $location ) );
+	exit;
+ 
+}
+ 
+/*
+ * Notices
+ */
+add_action('admin_notices', 'isocheck_custom_order_status_notices');
+ 
+function isocheck_custom_order_status_notices() {
+ 
+	global $pagenow, $typenow;
+ 
+	if( $typenow == 'shop_order' 
+	 && $pagenow == 'edit.php'
+	 && isset( $_REQUEST['marked_awaiting_shipment'] )
+	 && $_REQUEST['marked_awaiting_shipment'] == 1
+	 && isset( $_REQUEST['changed'] ) ) {
+ 
+		$message = sprintf( _n( 'Order status changed.', '%s order statuses changed.', $_REQUEST['changed'] ), number_format_i18n( $_REQUEST['changed'] ) );
+		echo "<div class=\"updated\"><p>{$message}</p></div>";
+ 
+	}
+ 
+}
